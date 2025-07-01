150407064e74deda9b1d8535b00db3b0ee421595
Hp <adkhamjon.abd@gmail.com>
2025-07-01 19:00:04 +0200

    Change the UserRepository to repository directory

--- src/main/java/com/adam/user_registration_notification_system/repository/UserRepository.java
+++ src/main/java/com/adam/user_registration_notification_system/repository/UserRepository.java
@@ -1,5 +1,6 @@
-package com.adam.user_registration_notification_system.model;
+package com.adam.user_registration_notification_system.repository;
 
+import com.adam.user_registration_notification_system.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
