ca12841a0a8133bb30b0e1d1aa6ace35f96fac05
Hp <adkhamjon.abd@gmail.com>
2025-07-01 18:52:55 +0200

    First commit with user entity

---
+++ src/main/java/com/adam/user_registration_notification_system/UserRegistrationNotificationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.adam.user_registration_notification_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegistrationNotificationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRegistrationNotificationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adam/user_registration_notification_system/UserRegistrationNotificationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adam.user_registration_notification_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegistrationNotificationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
