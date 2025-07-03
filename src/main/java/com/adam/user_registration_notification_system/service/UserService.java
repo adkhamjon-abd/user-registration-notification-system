b8e382079530db1fc1d49a4a418b78c8bae15a66
Hp <adkhamjon.abd@gmail.com>
2025-07-03 23:00:31 +0200

    Fix some changes in pom file to make lombok work and finaly add the eventpublisher to UserService class to connect everything

--- src/main/java/com/adam/user_registration_notification_system/service/UserService.java
+++ src/main/java/com/adam/user_registration_notification_system/service/UserService.java
@@ -1,8 +1,10 @@
 package com.adam.user_registration_notification_system.service;
 
+import com.adam.user_registration_notification_system.UserRegisteredEvent;
 import com.adam.user_registration_notification_system.model.User;
 import com.adam.user_registration_notification_system.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,8 +13,11 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    private ApplicationEventPublisher eventPublisher;
     public User register(User user){
         System.out.println("service accepted");
+        eventPublisher.publishEvent(new UserRegisteredEvent(this, user));
         return userRepository.save(user);
     }
 }
